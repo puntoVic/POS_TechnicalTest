7c64efd5c1400ba46091530a50394ca73e5106e2
Victor Olvera <volvera.thome@gmail.com>
2023-01-10 22:22:04 -0600

    create project

---
+++ POS_CASHMasters/CalculusFunctions/ChangeCalculation.cs
@@ -0,0 +1,12 @@
+﻿using CalculusFunctions.Contracts;
+
+namespace CalculusFunctions
+{
+    public static class ChangeCalculation : IChangeCalculation
+    {
+        Dictionary<int, int> CalculateChange(Dictionary<int, int> amountPayment, decimal price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
